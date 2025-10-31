9648b94167dde0cc3f7f9997f2f3efa79315ee12
axelmorales08 <axel08morales@emailcom>
2025-10-31 11:55:00 -0300

    backend teacher

--- api/controllers/teacherControllers.js
+++ api/controllers/teacherControllers.js
@@ -1,5 +1,5 @@
 // ===================== Importaciones =====================
-const { User, Course, ClassSection, Subject } = require('../models');
+const { User, Course, ClassSection, Subject, ClassSubject, StudentClass } = require('../models');
 const bcrypt = require('bcrypt');
 
 // ===================== Controladores =====================
@@ -34,11 +34,60 @@ const teacherCourses = async (req, res) => {
     const { teacherId } = req.params;
 
     try {
-        const courses = await Course.findAll({
-            where: { teacherId },
-            include: [ClassSection, Subject] // Incluye secciones y asignaturas relacionadas
+        const courses = await ClassSection.findAll({
+            where: { teacherId:  teacherId}
         });
+
+        if (!courses)
+      return res.status(400).json('No hay cursos registrados');
+
         res.json(courses);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Error interno del servidor', error: err.message });
+    }
+};
+
+// Obtener materias de un profesor
+const teacherSubject = async (req, res) => {
+    const { ClassSection_id } = req.params;
+
+    try {
+        const subjects = await ClassSubject.findAll({
+            where: { class_section_id:  ClassSection_id},
+            include:  [
+                { model: Subject, as: 'subject' } 
+            ]
+        });
+
+        if (!subjects)
+      return res.status(400).json('No hay sectiones de materia registrado');
+
+        if(subjects.length > 1) return res.status(300).json(subjects);
+
+        res.status(417).json(subjects)
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Error interno del servidor', error: err.message });
+    }
+};
+
+// Obtener alumnos de la seccion de un profesor
+const teacherStudents = async (req, res) => {
+    const { ClassSection_id } = req.params;
+
+    try {
+        const Students = await StudentClass.findAll({
+            where: { class_sections_id:  ClassSection_id},
+            include:  [
+                { model: User, as: 'student' } 
+            ]
+        });
+
+        if (!Students)
+      return res.status(400).json('No hay estudiantes en la seccion registrados');
+
+        res.status(417).json(Students)
     } catch (err) {
         console.error(err);
         return res.status(500).json({ message: 'Error interno del servidor', error: err.message });
