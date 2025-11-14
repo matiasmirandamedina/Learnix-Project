d5f3c9892b62e8081b31a9e022e7437a7db0dc18
william <azucarpochoclo@gmail.com>
2025-11-14 11:50:13 -0300

    arreglod de errores admin

--- client/src/components/homePages/homeAdmin.jsx
+++ client/src/components/homePages/homeAdmin.jsx
@@ -1,21 +1,35 @@
-import { search } from '../../../../api/routes/teacherRoutes';
 import NavBarAdmin from '../navBarPages/NavBarAdmin'
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 function HomeAdmin() {
-  const [activos, setActivos] = useState();
+  const [activos, setActivos] = useState(0);
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   const fetchUsers = async () => {
-    const response = await axios.get("http://localhost:3000/api/getUser")
-    setActivos(response)
+    const response = await axios.get("http://localhost:3000/api/admin/users",
+      {
+        headers: {
+          Authorization: token
+        }
+      }
+    );
+
+    console.log(response.data);
+    setActivos(response.data.cantidadActivos);
   };
 
   return (
     <>
-        <NavBarAdmin/>
-        <div className="admin-page">
-          <h1>Panel del Administrador</h1>
-          <p>Usuarios activos: {activos}</p>
-        </div>
+      <NavBarAdmin />
+      <div className="admin-page">
+        <h1>Panel del Administrador</h1>
+        <p>Usuarios activos: {activos}</p>
+      </div>
     </>
   );
 }
